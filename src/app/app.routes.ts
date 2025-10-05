a7c4a040c7c7657c84708ff36976dba2dc1571cd
Márcio José Mariano <marciojmariano@gmail.com>
2025-10-05 09:37:58 -0300

    Inicio da atividade para criacação do Aluno

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,8 @@
 import { Routes } from '@angular/router';
 import { Pessoa } from './pages/pessoa/pessoa';
+import { Aluno } from './pages/aluno/aluno';
 
 export const routes: Routes = [
-
     { path: "pessoa", component: Pessoa },
+    { path: "aluno", component: Aluno }
 ];
---
+++ src/app/pages/aluno/aluno.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-aluno',
+  imports: [FormsModule],
+  templateUrl: './aluno.html',
+  styleUrl: './aluno.scss'
+})
+export class Aluno {
+
+}
