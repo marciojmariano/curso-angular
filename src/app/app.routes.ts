3bb4032ed7dda6c213b914d7e6ab92a595acc582
Márcio José Mariano <marciojmariano@gmail.com>
2025-10-05 08:38:31 -0300

    Exemplos Angular Primeira Aula

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Pessoa } from './pages/pessoa/pessoa';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    { path: "pessoa", component: Pessoa },
+];
---
+++ src/app/pages/pessoa/pessoa.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-pessoa',
+  imports: [FormsModule],
+  templateUrl: './pessoa.html',
+  styleUrl: './pessoa.scss'
+})
+export class Pessoa {
+  // Propriedade
+  titulo: string = "Exemplo 1: Apresentação de variável"
+  quantidade: number = 0
+  nomeCompleto?: string | null
+  nome: string = ""
+  sobrenome: string = ""
+  idade: number = 0
+  anoNascimento?: number | null
+
+  // Método sem retorno
+  alterarTitulo(): void {
+    // let nome = "Francisco";
+    // console.log(nome);
+    // alert(this.titulo)
+    this.titulo = "Apresentação de variável"
+  }
+  incrementar(): void {
+    this.quantidade = this.quantidade + 1
+  }
+  concatenar(): void {
+    //let nome: string = "Márcio";
+    //let sobrenome: string = "Mariano";
+    // let nomeCompleto: string = nome + " " + sobrenome;
+    let nomeCompleto: string = this.nome + " " + this.sobrenome;
+    this.nomeCompleto = nomeCompleto;
+
+    this.anoNascimento = 2025 - this.idade
+
+
+  }
+}
