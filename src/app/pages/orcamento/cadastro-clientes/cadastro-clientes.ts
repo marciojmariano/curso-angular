adfb5042a71ee7760d2a62b253664d80b87ba1e2
Márcio José Mariano <marciojmariano@gmail.com>
2025-10-18 20:35:24 -0300

    atualização

--- src/app/pages/orcamento/cadastro-clientes/cadastro-clientes.ts
+++ src/app/pages/orcamento/cadastro-clientes/cadastro-clientes.ts
@@ -4,15 +4,17 @@ import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro-clientes',
-  standalone: true, // Indica que este é um Standalone Component
+  standalone: true, // Define que este é um Standalone Component
   imports: [CommonModule, ReactiveFormsModule], // Importa ReactiveFormsModule aqui
   templateUrl: './cadastro-clientes.html',
   styleUrls: ['./cadastro-clientes.scss']
 })
 export class CadastroClientes {
   clienteForm: FormGroup;
+  clientes: any[] = []; // Lista de clientes cadastrados
 
   constructor(private fb: FormBuilder) {
+    // Inicializa o formulário
     this.clienteForm = this.fb.group({
       nome: ['', [Validators.required, Validators.minLength(3)]],
       dataNascimento: ['', Validators.required],
@@ -23,14 +25,26 @@ export class CadastroClientes {
     });
   }
 
+  ngOnInit(): void {
+    this.consultarClientes(); // Carrega os clientes cadastrados
+  }
+
   salvarCliente() {
     if (this.clienteForm.valid) {
       const clienteData = this.clienteForm.value;
-      localStorage.setItem('cliente', JSON.stringify(clienteData));
+      const clientesExistentes = JSON.parse(localStorage.getItem('clientes') || '[]');
+      clientesExistentes.push(clienteData);
+      localStorage.setItem('clientes', JSON.stringify(clientesExistentes));
+      this.consultarClientes();
       alert('Cliente salvo com sucesso!');
       this.clienteForm.reset();
     } else {
       alert('Por favor, preencha todos os campos obrigatórios corretamente.');
     }
+  }
+
+  consultarClientes() {
+    const clientesCadastrados = JSON.parse(localStorage.getItem('clientes') || '[]');
+    this.clientes = clientesCadastrados;
   }
 }
--- src/app/pages/orcamento/orcamento.ts
+++ src/app/pages/orcamento/orcamento.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
-import { RouterLink, RouterOutlet } from '@angular/router';
 
 
 @Component({
   selector: 'app-orcamento',
-  imports: [FormsModule, CommonModule, RouterOutlet, RouterLink],
+  imports: [FormsModule, CommonModule],
   templateUrl: './orcamento.html',
   styleUrl: './orcamento.scss'
 })
